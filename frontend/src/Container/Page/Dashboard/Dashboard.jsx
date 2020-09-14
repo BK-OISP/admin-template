f71e15f3ac0ad4a4766b5cdb11204b2c7fd59c6a
kevin <dqtkien@gmail.com>
2020-09-14 09:20:17 +0700

    hello world

---
+++ frontend/src/Container/Page/Dashboard/Dashboard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AdminLayout from "../../Layout/AdminLayout";
+
+const Dashboard = (props) => {
+  return <AdminLayout>Check</AdminLayout>;
+};
+
+export default Dashboard;
