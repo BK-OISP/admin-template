325e2b682652c58ecc21e2100f2a08d85d510451
kevin <dqtkien@gmail.com>
2020-09-15 11:14:53 +0700

    add sidebar

--- frontend/src/Container/Compoment/Sidebar/Sidebar.jsx
+++ frontend/src/Container/Compoment/Sidebar/Sidebar.jsx
@@ -1,7 +1,44 @@
 import React from "react";
 
 const Sidebar = () => {
-  return <div className="sidebar">sidebar</div>;
+  return (
+    <div className="sidebar">
+      <ul className="sidebar-nav">
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Dashboard</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Dashboard</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Dashboard</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Dashboard</span>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
