5463b0c3e3fbc2d8ba5279ed0066972bd7b9e82d
kevin <dqtkien@gmail.com>
2020-09-15 15:16:25 +0700

    add scrollbar

--- frontend/src/Container/Compoment/Sidebar/Sidebar.jsx
+++ frontend/src/Container/Compoment/Sidebar/Sidebar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
+    <div className="sidebar overlay-scrollbar">
       <ul className="sidebar-nav">
         <li className="sidebar-nav--item">
           <div className="sidebar-nav--link active">
@@ -18,6 +18,126 @@ const Sidebar = () => {
               <i className="material-icons">dashboard</i>
             </div>
             <span>Quản lý tài sản</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Quản lý tài sản</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Quản lý tài sản</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Quản lý tài sản</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Quản lý tài sản</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Quản lý tài sản</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Quản lý tài sản</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Quản lý tài sản</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Quản lý tài sản</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Quản lý tài sản</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Quản lý tài sản</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Quản lý tài sản</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Dashboard</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Dashboard</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Dashboard</span>
+          </div>
+        </li>
+        <li className="sidebar-nav--item">
+          <div className="sidebar-nav--link">
+            <div>
+              <i className="material-icons">dashboard</i>
+            </div>
+            <span>Dashboard</span>
           </div>
         </li>
         <li className="sidebar-nav--item">
